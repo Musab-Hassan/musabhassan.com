63a60b497dbcfdd90ea996fe1799159d385d422b
Musab Hassan <musab@Musabs-MacBook-Pro.local>
2021-09-04 16:00:14 -0700

    Create sections

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
-	export let name;
+	
+	import HomeSection from "sections/home.svelte";
+	import WorkSection from "sections/work.svelte";
+	import AboutSection from "sections/about.svelte";
+
 </script>
 
 <style lang="sass">
-	
 </style>
+
+<HomeSection></HomeSection>
+<WorkSection></WorkSection>
+<AboutSection></AboutSection>
 
---
+++ src/sections/about.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<style lang="sass">
+</style>
+
