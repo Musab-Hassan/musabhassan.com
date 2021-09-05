858bddee9a156c02c6728098b83484b0a4047f63
Musab Hassan <musab@Musabs-MacBook-Pro.local>
2021-09-04 17:51:49 -0700

    Create navigation bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
 	
-	import HomeSection from "sections/home.svelte";
-	import WorkSection from "sections/work.svelte";
-	import AboutSection from "sections/about.svelte";
+	import HomeSection from "./sections/home.svelte";
+	import WorkSection from "./sections/work.svelte";
+	import AboutSection from "./sections/about.svelte";
+	import NavComponent from "./components/nav.svelte"
 
 </script>
 
 <style lang="sass">
+
+	
+\:global(body)
+	background-color: #2A2A2C
+	overflow: hidden
+	color: white
+
+\:global(*)
+	margin: 0
+	paddding: 0
+	-moz-osx-font-smoothing: grayscale;
+	-webkit-font-smoothing: antialiased;
+	will-change: opacity;
+
 </style>
 
+<NavComponent></NavComponent>
 <HomeSection></HomeSection>
 <WorkSection></WorkSection>
 <AboutSection></AboutSection>
--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -2,5 +2,143 @@
 </script>
 
 <style lang="sass">
+
+@import "../../consts.sass"
+    
+.nav-wrapper #nav-container, #content-container .year
+	display: none !important
+
+.nav-wrapper
+	width: 100vw
+	position: fixed
+	margin: 0 auto
+	top: 10vh
+	padding: 0 7vw
+	box-sizing: border-box
+	display: block
+	z-index: 30
+	display: flex
+	flex-direction: row
+	justify-content: space-between
+	align-items: center
+
+	.logo-icon
+		display: inline-block
+		height: 4vh
+
+	ul.nav-list
+		list-style-type: none
+
+		li
+			font-family: $font
+			text-transform: lowercase
+			font-size: 2.5vh
+			display: inline-block
+
+			a
+				color: white
+				text-decoration: none
+
+			&:not(:last-child)::after
+				margin-right: 0.3vw
+				margin-left: 0.5vw
+				content: "-"
+
+	.hb-button
+		cursor: pointer
+		position: relative
+		z-index: 21
+
+		*
+			display: inline-block
+			vertical-align: middle
+
+		.text
+			font-family: $font
+			font-size: 2.5vh
+			text-transform: lowercase
+
+		.hb
+			width: 3vh
+			height: 2.1vh
+			margin-right: 1.5vh
+
+			span
+				transition: 0.8s ease
+				-webkit-transition: 0.8s ease
+				-moz-transition: 0.8s ease
+				display: block
+				position: relative
+				top: 0
+				right: 0
+				height: 2px
+				width: 100%
+				background-color: white
+
+				&:nth-child(1)
+					top: 0
+
+				&:nth-child(2)
+					top: 0.5vh
+
+				&:nth-child(3)
+					top: 1.1vh
+
+		&.close
+			.text
+				color: white
+
+			.hb
+				span
+					background-color: white
+					top: 0.5vh
+
+					&:nth-child(1)
+						transform: translateY(100%) rotate(-45deg)
+						width: 100%
+
+					&:nth-child(2)
+						width: 0
+
+					&:nth-child(3)
+						transform: translateY(-100%) rotate(45deg)
+						width: 100%
+
+@media only screen and (min-width: 950px)
+	.hb-button
+		display: none
+
+	.nav-list
+		display: block
+
+@media only screen and (max-width: 950px)
+	.hb-button
+		display: block
+
+	.nav-list
+		display: none
 </style>
 
+<div class="nav-wrapper">
+	<div class="flex-wrapper">
+		<img draggable="false" data-name="home" src="assets/imgs/logo.svg" alt="Page Logo" style = "width:7vh;" class = "logo-icon clickable">
+	</div>
+    
+	<div class="flex-wrapper">
+		<ul class="nav-list">
+			<li>Home</li>
+			<li>Work</li>
+			<li>About</li>
+			<li><a href="mailto:musabhassan04@gmail.com" target="_blank">Email</a></li>
+		</ul>
+
+		<div class="hb-button clickable">
+			<div class="hb">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div class="text">Menu</div>
+		</div>
+	</div>
+</div>
