8cb8bd7ca67da33c3759b0fe04e62a2284de5355
Musab Hassan <musabhassan04@gmail.com>
2022-01-05 23:15:37 -0800

    Add missing stores

--- src/store.ts
+++ src/store.ts
@@ -3,6 +3,7 @@ import { writable } from "svelte/store";
 // Clickables for cursor dot
 export let clickables = writable([]);
 export let isWorkScroll = writable(false);
+export let workScrollPosition = writable(0);
 
 // Artificial page load time
 export let waitTime = 1000;
