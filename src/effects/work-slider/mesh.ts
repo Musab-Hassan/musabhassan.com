fe8fbd783922e00d515000c25ea2a64e75b03745
Musab Hassan <musabhassan04@gmail.com>
2022-01-08 02:46:03 -0800

    Correct imageTexture scaling

--- src/effects/work-slider/mesh.ts
+++ src/effects/work-slider/mesh.ts
@@ -5,8 +5,22 @@ import { fragmentShader, vertexShader } from "./shaders";
 
 export class MeshItem {
 
-    element; scene; offset; sizes; geometry; imageTexture; uniforms; material; mesh;
+    // TODO: Fix for ultrawide screens
+
+    // Mesh and geometry
+    element; scene; offset; sizes; geometry; uniforms; material; mesh;
+    // Speed of workScroller
     speed: number;
+
+    // Asyncronous image loading
+    resolveImagePromise;
+    imageLoadPromise = new Promise(r => {
+        this.resolveImagePromise = r;
+    });
+
+    // Texture data
+    image: HTMLImageElement;
+    imageTexture: THREE.CanvasTexture;
 
     // Pass in the scene as we will be adding meshes to this scene.
     constructor(element, scene) {
@@ -26,15 +40,32 @@ export class MeshItem {
     setDimensions() {
         const { width, height, left } = this.element.parentElement.getBoundingClientRect();
         this.sizes.set(width, height);
-        this.offset.set(left, 0);
+        this.offset.set(left - width * 1.5, 0);
     }
 
-    createMesh() {
-        this.geometry = new THREE.PlaneBufferGeometry(1, 1, 100, 100);
-        this.imageTexture = new THREE.TextureLoader().load(this.element.src);
+    async createMesh() {
+        this.geometry = new THREE.PlaneBufferGeometry(1, 1, 15, 15);
+
+        this.imageTexture = new THREE.TextureLoader().load(this.element.src, (t) => {
+            t.wrapS = THREE.MirrorRepeating;
+            t.wrapT = THREE.MirrorRepeating;
+            
+            this.resolveImagePromise(); // Allow render to run
+        });
+
+
+        this.setDimensions();
+        const imgBoundBox = this.element.getBoundingClientRect();
+
         this.uniforms = {
             uTexture: { //Texture
                 value: this.imageTexture
+            },
+            uMeshSize: {
+                value: new THREE.Vector2(this.sizes.x, this.sizes.y)
+            },
+            uImgSize: {
+                value: new THREE.Vector2(imgBoundBox.width, imgBoundBox.height)
             },
             uOffset: { //Distortion strength
                 value: new THREE.Vector2(0.0, 0.0)
@@ -43,25 +74,31 @@ export class MeshItem {
                 value: 0.7
             }
         };
+
         this.material = new THREE.ShaderMaterial({
             uniforms: this.uniforms,
             vertexShader: vertexShader,
             fragmentShader: fragmentShader,
-            transparent: true,
-            side: THREE.DoubleSide
-        })
+            transparent: true
+        });
+
         this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.setDimensions(); // set offsetand sizes for placement on the scene
         this.mesh.position.set(this.offset.x, this.offset.y, 0);
         this.mesh.scale.set(this.sizes.x, this.sizes.y, 1);
 
         this.scene.add(this.mesh);
     }
 
-    render() {
+    async render() {
+        await this.imageLoadPromise;
+
         this.setDimensions();
-        this.mesh.position.set(this.offset.x, this.offset.y, 0)
-        this.mesh.scale.set(this.sizes.x, this.sizes.y, 1)
-        this.uniforms.uOffset.value.set(this.speed * -0.0003, this.offset.y * 0.0)
+        this.mesh.position.set(this.offset.x, this.offset.y, 0);
+        this.mesh.scale.set(this.sizes.x, this.sizes.y, 1);
+        // Warping and Distortion effect
+        this.uniforms.uOffset.value.set(this.speed * -0.0003, this.speed * -0.00003); 
+        // Object-fit cover effect
+        this.uniforms.uMeshSize.value.set(this.sizes.x, this.sizes.y);
     }
 }
+
--- src/effects/work-slider/shaders.ts
+++ src/effects/work-slider/shaders.ts
@@ -31,16 +31,22 @@ export var fragmentShader = `
 uniform sampler2D uTexture;
 uniform float uAlpha;
 uniform vec2 uOffset;
+uniform vec2 uMeshSize;
+uniform vec2 uImgSize;
 varying vec2 vUv;
 
 vec3 rgbShift(sampler2D textureImage, vec2 uv, vec2 offset) {
-   float r = texture2D(textureImage,uv + offset).r;
-   vec2 gb = texture2D(textureImage,uv).gb;
+
+   float centeredX = ((uv.x - (uImgSize.x / uImgSize.y / 2.0)) * uMeshSize.x / uMeshSize.y) - uImgSize.x;
+   vec2 pos = vec2(centeredX, uv.y);
+
+   float r = texture2D(textureImage,pos + offset).r;
+   vec2 gb = texture2D(textureImage,pos).gb;
    return vec3(r,gb);
 }
 
 void main() {
-   vec3 color = rgbShift(uTexture,vUv,uOffset);
+   vec3 color = rgbShift(uTexture, vUv, uOffset);
    gl_FragColor = vec4(color,uAlpha);
 }
 
