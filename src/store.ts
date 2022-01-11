2c73b84b27b001e5fdc00a39c53b9cfdb13426b2
Musab Hassan <musabhassan04@gmail.com>
2022-01-10 20:19:13 -0800

    Add load time

--- src/store.ts
+++ src/store.ts
@@ -4,4 +4,4 @@ export let clickables = writable([]); // Clickables for cursor dot
 export let isWorkScroll = writable(false); // is work slider being scrolled
 export let workScrollSpeed = writable(0); // Speed of work slider
 
-export let waitTime = 0; // Artificial page load time
+export let waitTime = 500; // Artificial page load time
