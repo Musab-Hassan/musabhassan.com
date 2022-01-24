2aa21a84c7cf8816982f9de7a266d8c1d28cb64c
Musab Hassan <musabhassan04@gmail.com>
2022-01-23 22:49:46 -0800

    Add string functions

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,15 @@ const app = new App({
 	target: document.body
 });
 
+// String additions
+declare global {
+	interface String {
+		insert(index: number, string: string): string
+	}
+}
+
+String.prototype.insert = function (i, str) {
+	return this.slice(0, i) + str + this.slice(i);
+};
+
 export default app;
