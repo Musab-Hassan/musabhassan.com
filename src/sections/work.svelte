62944193421faf2c2a27801bee4d4853c0dc9b98
Musab Hassan <musabhassan04@gmail.com>
2022-01-24 16:35:58 -0800

    Add work details view to main slider

--- src/sections/work.svelte
+++ src/sections/work.svelte
@@ -3,81 +3,90 @@
 import { onMount } from "svelte";
 import { clickables, isWorkScroll, workScrollSpeed } from "../store";
 import { ImageRenderer } from "../effects/work-slider/renderer";
-
-let isHold = false; // is user holding click
-let _viewLinks = []; // Array of clickable Links
-let container, listContainer; // Binds to containers
-let images = []; // Array of images to be passed to WebGL Shader
-
-isWorkScroll.subscribe(v => {
-	isHold = v;
-});
-
+import { letterSlide, maskSlide } from "../utils";
+import { fade } from "svelte/transition";
 
 // Slider calculations and rendering
-let currentMouseX = 0, initialMouseX = 0;
-let currentPosition = 0, initialPosition = 0;
-let offsetSpeed = 4000, lerpSpeed = 0.075;
-
 class WorkSlider {
+	currentMouseX = 0; initialMouseX = 0;
+	currentPosition = 0; targetPosition = 0; initialPosition = 0;
+	offsetSpeed = 4000; 
+	lerpSpeed = 0.075;
 
-    onInitHold(e) {
-        if (isHold) return;
+    onHold = e => {
+        if (currentActive != null || isMouseDown || e.target.classList.contains("button")) return;
 
-        initialMouseX = e.clientX;
+        this.initialMouseX = e.clientX;
+		this.currentMouseX = e.clientX;
         isWorkScroll.set(true);
 
-        if (isHold) {
+        if (isMouseDown) {
             let style = window.getComputedStyle(listContainer);
             let matrix = new WebKitCSSMatrix(style.transform);
-            initialPosition = matrix.m41;
+            this.initialPosition = matrix.m41;
         }
     }
 
     onRelease() {
         isWorkScroll.set(false);
     }
+ 
+    onMouseMove = e => {
+    	if (!isMouseDown) return; 
+		this.currentMouseX = e.clientX;
 
-    onMouseMove(e) {
-        if (isHold) currentMouseX = e.clientX;
+        let diff = (this.currentMouseX - this.initialMouseX) * -1;
+		this.targetPosition = this.initialPosition - (this.offsetSpeed * (diff / document.body.clientWidth));
     }
 
-    animate() {
-        let endPoint = listContainer.offsetWidth - document.body.clientWidth
-        if (endPoint < 0) endPoint = listContainer.offsetWidth;
+    animate = () => {
+		if (currentActive === null) {
+			let endPoint = listContainer.offsetWidth - document.body.clientWidth
+			if (endPoint < 0) endPoint = listContainer.offsetWidth;
 
-        let diff = (currentMouseX - initialMouseX) * -1;
-		let targetPosition = initialPosition - (offsetSpeed * (diff / document.body.clientWidth));
-
-        // Checks for disabling overscrolling
-        if (targetPosition > 0) targetPosition = 0;
-        if (targetPosition <= (endPoint * -1)) targetPosition = -endPoint;
+			// Checks for disabling overscrolling
+			if (this.targetPosition > 0) this.targetPosition = 0;
+			if (this.targetPosition <= (endPoint * -1)) this.targetPosition = - endPoint;
+		}
 
         // Lerp easing
-        currentPosition = lerp(currentPosition, targetPosition, lerpSpeed);
+        this.currentPosition = this.lerp(this.currentPosition, this.targetPosition, this.lerpSpeed);
 		
-        // Svelte store value for the Canvas effect
-        workScrollSpeed.set(currentPosition - targetPosition);
+        workScrollSpeed.set(this.currentPosition - this.targetPosition); // Set Svelte Store value for the Canvas effect
+        listContainer.style.transform = `translateX(${this.currentPosition}px)`;
 
-        listContainer.style.transform = `translateX(${currentPosition}px)`;
-
-        requestAnimationFrame(this.animate.bind(this));
+        requestAnimationFrame(this.animate);
     }
+
+	lerp(start, end, t) {
+		return start * (1 - t) + end * t;
+	}
 }
 
-function lerp(start, end, t) {
-    return start * (1 - t) + end * t;
-}
 
+let isMouseDown = false; // is user holding click
+let _viewLinks = []; // Array of clickable Links
+let container, listContainer; // Binds to containers
+let images = []; // Array of images to be passed to WebGL Shader
+let workItems = []; // Array of workItems to be animated
+let currentActive = null; // Active work item in the detailsViewer viewed
+let data; // JSON Work data fetched from the data.json file
 
+// Animations for work description
+let textAnimationIn = letterSlide().in;
+let textAnimationOut = letterSlide().out;
+let maskAnimationIn = maskSlide().in;
+let maskAnimationOut = maskSlide().out;
 
+// Svelte Store subscriptions
+isWorkScroll.subscribe(val => isMouseDown = val);
 
-// Fetch work data
+const slider = new WorkSlider(); // Slider effects
+
 const workItemsFetch = new Promise(async (resolve: (data: any[]) => void) => {
-	resolve(await (await fetch("data.json")).json());
+	data = await (await fetch("data.json")).json(); // Fetch work data
+	resolve(data);
 });
-
-const slider = new WorkSlider();
 
 onMount(async () => {
 	listContainer.style.transform = "translate3d(0px, 0px, 0px)";
@@ -90,45 +99,122 @@ onMount(async () => {
 	new ImageRenderer(container, images); // ThreeJS warping effect
 });
 
+
+
+// Helper Functions
+function toggleActiveItem(i) {
+	currentActive = (currentActive == i) ? null : i;
+	if (currentActive != null) slider.targetPosition = -(workItems[i].offsetLeft - (window.innerWidth / 4) + window.innerWidth / 10);
+}
+
+// Add clickables dynamically
+function addClickable(node) {
+	clickables.update(values => [...values, node]);
+	return {
+		destroy() {
+			clickables.update(values => values.filter(item => item !== node));
+		}
+	}
+}
+
+
 </script>
 
 
 
-<div id="content-container" class="work-click-area" style = "margin-top:20vh;">
+<div id="content-container" class="work-click-area" style = "margin-top: 30vh;">
 	<div class="content-wrapper" 
-		on:mousedown={slider.onInitHold}
+		on:mousedown|preventDefault={slider.onHold}
 		on:mouseup={slider.onRelease}
 		on:mouseleave={slider.onRelease}
 		on:mousemove|preventDefault={slider.onMouseMove}
 		bind:this={container}
+		class:disabled={currentActive !== null}
 	>
-		<ul class="work-list" bind:this={listContainer} class:isHold>
+		<ul class="work-list" bind:this={listContainer} class:hold={isMouseDown}>
 			<!-- Work items render here -->
 			{#await workItemsFetch then items}
 				{#each items as item, i}
-					<li class="list-item clickable passive" data-id="{item.id}">
+					<li class="list-item clickable passive" 
+						class:ambient="{currentActive !== i && currentActive !== null}" 
+						class:active="{currentActive === i}" 
+						bind:this={workItems[i]}>
+
 						<div class="img-wrapper">
 							<img bind:this={images[i]}
 								on:dragstart|preventDefault 
 								draggable="false" 
 								src="assets/imgs/work-back/{item.id}/cover.jpg" 
-								alt="{item.title} Background Image">
+								alt="{item.title} Background">
 						</div>
-						<div class="text-top-wrapper">
+						<div class="text-top-wrapper" class:hidden={currentActive != null || isMouseDown}>
 							<p class="item-date">{item.date}</p>
 							<!-- <p class="item-summary">{item.summary}</p> -->
 						</div>
-						<div class="text-wrapper">
+						<div class="text-wrapper" class:hidden={currentActive != null || isMouseDown}>
 							<h1 class="item-title">{item.title}</h1>
-							<div class="button" bind:this={_viewLinks[i]}>view</div>
+							<div class="button" bind:this={_viewLinks[i]} on:click={() => toggleActiveItem(i)}>view</div>
 						</div>
 					</li>
 				{/each}
 			{/await}
 		</ul>
+
+		<!-- Active item details (When a work item is clicked) -->
+		{#if currentActive !== null}
+			<div class="details-container">
+				<div class="wrapper">
+
+					<div class="top-align">
+						<div class="wrapper">
+							<div class="index">
+								<div in:maskAnimationIn out:maskAnimationOut>
+									{#if (currentActive < 9)}
+										{"0"+data[currentActive].id}
+									{:else}
+										{data[currentActive].id}
+									{/if}
+								</div>
+							</div>
+							<span class="line" transition:fade></span>
+							<h6 class="caption">
+								<div in:maskAnimationIn out:maskAnimationOut>{data[currentActive].details.summary}</div>
+							</h6>
+						</div>
+					</div>
+					
+					<div class="mid-align">
+						<h1 class="title" in:textAnimationIn={{ breakWord: false }} out:textAnimationOut>{data[currentActive].title}</h1>
+						<div class="button" use:addClickable on:click={() => toggleActiveItem(currentActive)} in:maskAnimationIn out:maskAnimationOut>close</div>
+					</div>
+					
+					<div class="bottom-align">
+						<div class = "paragraph">
+							<p in:maskAnimationIn out:maskAnimationOut>
+								{data[currentActive].details.description}
+							</p>
+						</div>
+						<div class="roles">
+							<div class="wrapper">
+								<p class="descriptor" in:textAnimationIn out:textAnimationOut>Role</p>
+								<ul>
+									{#each data[currentActive].roles as role}
+										<li in:textAnimationIn out:textAnimationOut>{"+ " + role}</li>
+									{/each}
+								</ul>
+							</div>
+						</div>
+						<div class="links">
+							{#each data[currentActive].links as link}
+								<a in:maskAnimationIn out:maskAnimationOut use:addClickable href={link.link} target="_blank" class="button">{link.text}</a><br>
+							{/each}
+						</div>
+					</div>
+				</div>
+			</div>
+		{/if}
 	</div>
 </div>
-
 
 
 <style lang="sass">
@@ -147,6 +233,9 @@ onMount(async () => {
 	flex-direction: column
 	cursor: grab
 	position: relative
+
+	&.disabled
+		cursor: default !important
 	
 	*
 		-webkit-touch-callout: none
@@ -154,6 +243,119 @@ onMount(async () => {
 		-moz-user-select: none
 		-ms-user-select: none
 		user-select: none
+
+	.details-container
+		position: absolute
+		left: 0
+		top: 0
+		height: 100%
+		width: 100%
+		display: flex
+		flex-direction: row
+		align-item: center
+		justify-content: space-between
+		box-sizing: border-box
+		padding: 0 14vw
+
+		.wrapper
+			text-align: left
+			position: relative
+			display: flex
+			flex-direction: column
+			justify-content: space-between
+
+			.top-align
+				.wrapper
+					display: inline-flex
+					flex-direction: row
+					align-items: center
+					justify-content: left
+					position: relative
+
+					h6.caption
+						position: relative
+						font-family: $font
+						text-transform: uppercase
+						font-weight: normal
+						font-size: 1.9vh
+
+					.index
+						font: $font
+						position: relative
+						font-size: 2.1vh
+
+					span.line
+						width: 300%
+						margin: 0 10%
+						height: 1.5px
+						background-color: white
+
+			.mid-align	
+				display: flex
+				flex-direction: row
+				align-items: center
+				justify-content: space-between
+
+				h1.title
+					position: relative
+					font-family: $titleFont
+					font-size: 7vw
+					text-transform: lowercase
+					word-wrap: break-word
+					white-space: normal
+
+				.button
+					font-size: 1.4vw
+					letter-spacing: 0.1vw
+					margin-top: 2vh
+					text-transform: uppercase
+
+			
+			.bottom-align
+				display: flex
+				flex-direction: row
+				justify-content: space-between
+				align-items: flex-start
+
+				*
+					flex-grow: 1
+					flex-basis: 0
+
+				.paragraph
+					font-size: 0.8vw
+					text-transform: uppercase
+
+				.roles 
+					display: flex
+					flex-direction: column
+					align-items: center
+
+					.descriptor
+						line-height: 250%
+						letter-spacing: 0.6vh
+						font-family: $font
+						text-transform: uppercase
+						font-weight: normal
+						font-size: 1.4vh
+
+
+					ul 
+						list-style-type: none
+
+						li
+							font-family: $font
+							text-transform: uppercase
+							font-weight: normal
+							font-size: 1.9vh
+							line-height: 160%
+
+				.links
+					position: relative
+
+					.button
+						font-size: 1.2vw
+						text-transform: uppercase
+						text-decoration: none
 
 	ul.work-list
 		margin-top: auto
@@ -166,30 +368,39 @@ onMount(async () => {
 		height: 70vh
 		min-width: min-content
 
-		&.isHold
+		&.hold
 			.list-item
-				height: 60vh !important
-			
-			.list-item .w-animate
-				opacity:0 !important
+				height: 50vh !important
 
 		.list-item
 			display: inline-flex
 			overflow: hidden
-			height: 70vh
-			width: 30vw
+			height: 60vh
+			width: 20vw
 			box-sizing: border-box
 			position: relative
 			overflow: hidden
 			z-index: 3
-			margin-right: 8vh
-			transition: width 0.8s cubic-bezier(0.25, 1, 0.5, 1), height 0.8s cubic-bezier(0.25, 1, 0.5, 1)
+			margin-right: 6vw
+			transition: width 0.8s cubic-bezier(0.25, 1, 0.5, 1), height 0.8s cubic-bezier(0.25, 1, 0.5, 1), margin 0.8s cubic-bezier(0.25, 1, 0.5, 1)
 
-			&.hover
-				height: 70vh
+			*
+				transition: opacity 0.3s ease
+				-webkit-transition: opacity 0.3s ease
 
-				img
-					opacity: 1
+			&.active
+				width: 50vw
+				margin-right: 16vw
+				margin-left: 10vw
+
+				.img-wrapper
+					width: 100%
+
+			&.ambient
+				height: 55vh
+
+			.hidden
+				opacity: 0
 
 			.img-wrapper
 				overflow: hidden
@@ -197,7 +408,6 @@ onMount(async () => {
 				z-index: 1
 				position: relative
 				width: 85%
-				border-radius: 0.2vh
 				box-shadow: 3px 9px 18px rgba(0, 0, 0, 0.2)
 				
 				img
@@ -205,8 +415,8 @@ onMount(async () => {
 					width: 110%
 					object-fit: cover
 					position: absolute
-					top: 50%;
-					left: 50%;
+					top: 50%
+					left: 50%
 					transform: translate(-50%, -50%)
 					-webkit-transform: translate(-50%, -50%)
 					opacity: 0.5
@@ -226,9 +436,9 @@ onMount(async () => {
 				text-align: right
 
 				.item-date
-					font-size: 1.4vw
+					font-size: 1vw
 					letter-spacing: 0.1vw
-					text-transform: lowercase
+					text-transform: uppercase
 
 			.text-wrapper
 				box-sizing: border-box
@@ -239,13 +449,15 @@ onMount(async () => {
 				z-index: 2
 
 				.button
-					font-size: 4vh
+					font-size: 1.4vw
+					letter-spacing: 0.1vw
 					margin-top: 2vh
+					text-transform: uppercase
 
 				.item-title
 					font-family: $font
 					font-weight: normal
-					font-size: 3.6vw
+					font-size: 2.3vw
 					z-index: 0
 					opacity: 1
 					letter-spacing: 0.1vw
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,181 @@
 
-// If client is a phone - This is unreliable but there I dont see a better solution
+import BezierEasing from "bezier-easing";
+
+// If client is a phone - It's unreliable but I dont have a better solution
 export function isUnsupportedClient(): boolean {
     let agent = navigator.userAgent || navigator.vendor;
     if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(agent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(agent.substring(0, 4))) {
         return true;
     }
     return false;
+}
+
+
+// Letter reveal animation used with the 'in:' and 'out:' svelte directives
+export function letterSlide() {
+    return {
+        in: (node, params: { duration?: number, delay?: number, initDelay?: number, breakWord?: boolean }) => {
+
+            if (!params.delay) params.delay = 35;
+            if (!params.initDelay) params.initDelay = 0;
+            if (!params.duration) params.duration = 600;
+            if (params.breakWord === undefined) params.breakWord = true;
+            
+            let masks = wordWrapHandler(node, params.breakWord);
+
+            // Set default properties
+            masks.forEach(e => {
+                e.childNodes.forEach(child => {
+                    child.style.transform = "translateX(150%)";
+                });
+                e.style.transform = "translateX(80%)";
+                e.style.display = "inline-flex";
+                e.style.overflow = "hidden";
+            });
+
+            return {
+                delay: params.initDelay,
+                duration: params.duration,
+                tick: t => {
+                    let eased = BezierEasing(.2, .58, .43, 1)(t);
+
+                    masks.forEach((e) => {
+                        let index = Array.from(e.parentNode.children).indexOf(e) + 1;
+
+                        e.childNodes.forEach(i => {
+                            setTimeout(() => {
+                                i.style.transform = `translateX(${(150 + (-eased * 150)).toFixed(2)}%)`;
+                            }, params.delay * index);
+                        });
+
+                        setTimeout(() => {
+                            e.style.transform = `translateX(${(80 + (-eased * 80)).toFixed(2)}%)`;
+                        }, params.delay * index);
+                    });
+                }
+            }
+        },
+
+        out: (node, params: { duration?: number, delay?: number, initDelay?: number, extraElems?: any }) => {
+            
+            let masks = node.querySelectorAll(".a-text-mask");
+            
+            if (!params.delay) params.delay = 25;
+            if (!params.initDelay) params.initDelay = 0;
+
+            return {
+                delay: params.initDelay,
+                duration: 600,
+                tick: t => {
+                    // let eased = BezierEasing(.59, .11, .07, 1.07)(t);
+                    let eased = BezierEasing(.32, .24, .76, .26)(t);
+
+                    masks.forEach((e) => {
+                        e.childNodes.forEach(i => {
+                            i.style.transform = `translateX(${(-150 + (eased * 150)).toFixed(2)}%)`;
+                        });
+
+                        e.style.transform = `translateX(${(-80 + (eased * 80)).toFixed(2)}%)`;
+                    });
+                }
+            }
+        }
+    }
+    
+
+    // Wrap each word with a mask and return masks
+    function wordWrapHandler(node, breakWord: boolean) {
+
+        let masks = node.querySelectorAll(".a-text-mask");
+
+        if (masks.length < 1) {
+            node.innerHTML = parseLetters(node.innerHTML, "<div class=\"a-text-mask\"><div class=\"a-text-block\">", "</div></div>");
+            masks = node.querySelectorAll(".a-text-mask");
+        }
+
+        if (breakWord) {
+            let words = node.querySelectorAll(".a-word");
+            words.forEach(element => {
+                element.style.display = "inline-block";
+                element.style.marginRight = "0.4vw"
+            });
+        }
+
+        return masks;
+
+        // parse the letters and break apart words
+        function parseLetters(string: string, startWord: string, endWord: string) {
+            let newString = "";
+            let isTag = false;
+            let isWord = false;
+
+            [...string].forEach((e, i) => {
+                if (e === " " || string[i-1] === " " || i === 0 || i === string.length) {
+                    isWord = !isWord;
+                    newString += isWord ? "<div class=\"a-word\">" : "</div>";
+                }
+                if (e == "<") isTag = true;
+                if (string[i-1] == ">") isTag = false;
+
+                if (!isTag && e !== " ") newString += startWord + e + endWord;
+            });
+
+            return newString;
+        }
+    }
+}
+
+
+// Mask reveal animation used with the 'in:' and 'out:' svelte directives
+export function maskSlide() {
+    return {
+        in: (node, params: { duration?: number, delay?: number}) => {
+
+            if (!params.delay) params.delay = 20;
+            if (!params.duration) params.duration = 700;
+
+            addMask();
+
+            return {
+                delay: params.delay,
+                duration: params.duration,
+                tick: t => {
+                    let eased = BezierEasing(.2, .58, .43, 1)(t);
+
+                    node.style.transform = `translateX(${(100 + (-eased * 100)).toFixed(2)}%)`; 
+                }
+            }
+
+            function addMask() {
+                let mask = document.createElement("div");
+                let parent = node.parentNode;
+                let index = Array.from(parent.children).indexOf(node);
+
+                mask.classList.add("a-mask");
+                node.classList.add("a-content");
+                mask.insertBefore(node, mask.children[0]);
+                mask.style.display = "inline-block";
+                mask.style.overflow = "hidden";
+                parent.insertBefore(mask, parent.children[index]);
+
+                node.style.transform = "translateX(100%)";
+            }
+        },
+
+        out: (node, params: { duration?: number, delay?: number }) => {
+            
+            if (!params.delay) params.delay = 0;
+            if (!params.duration) params.duration = 400;
+
+            return {
+                delay: params.delay,
+                duration: params.duration,
+                tick: t => {
+                    let eased = BezierEasing(.32, .24, .76, .26)(t);
+
+                    node.style.transform = `translateX(${(-100 + (eased * 100)).toFixed(2)}%)`;
+                }
+            }
+        }
+    }
 }
