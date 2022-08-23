8b56c034e2cf7ec254f146d2ac59d836b7c8e18f
Musab-Hassan <musabhassan04@gmail.com>
2022-08-23 08:49:46 -0700

    Major performance fix for cursor dot

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,6 @@ import Footer from "./components/footer.svelte";
 import CursorDot from "./components/cursor-dot.svelte"
 import Loader from "./components/loader.svelte";
 
-let trackedDot; // Mouse following dot
 let scrollContainer;
 let navBar;
 let loading = true;
@@ -49,16 +48,17 @@ onMount(async () => {
 
 
 <!-- Cursor dot tracking when mouse moves inside the body -->
-<svelte:body on:mousemove = {(e) => trackedDot.trackMouse(e)}/>
-<CursorDot bind:this={trackedDot}></CursorDot>
+<CursorDot></CursorDot>
 
 <!-- Page loading progress bar -->
 {#if loading} <Loader></Loader> {/if}
 
 <div id="scroll-frame" bind:this={scrollContainer}>
+	<!-- Top nav-bar and mobile nav-bar -->
 	<div id="nav-bar" bind:this={navBar}>
 		<NavComponent scrollContainer={scrollContainer}></NavComponent>
 	</div>
+	<!-- page sections -->
 	<HomeSection></HomeSection>
 	<WorkSection></WorkSection>
 	<AboutSection></AboutSection>
--- src/components/cursor-dot.svelte
+++ src/components/cursor-dot.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 
+import { onMount } from "svelte";
 import { clickables as clickableStore, isMobile, isWorkScroll } from "../store";
 
 let container;
@@ -10,105 +11,8 @@ let hover: boolean = false;
 let disabled: boolean = false;
 let introDisabled: boolean = true;
 
-let x = 0, y = 0;
-
-// Track the mouse with easing and handle hover effects
-export function trackMouse(e) {
-
-	// Dont do anything if its a touch mobile device
-	if ($isMobile) return;
-	// Intro animation for cursor dot
-	if (introDisabled) setTimeout(() => introDisabled = false, 500);
-
-	let active, t = 0;
-
-	let clickable = currentlyHoveredClickable(e);
-	hover = !!clickable;
-
-	// Clickale transition if hovering a clickable
-	if (clickable) {
-		let width = clickable.clientWidth;
-		let height = clickable.clientHeight;
-		
-		let clickableMid = {
-			x: (clickable.getBoundingClientRect().left + (width / 2)),
-			y: (clickable.getBoundingClientRect().top + (height / 2))
-		}
-
-		active = {
-			x: clickableMid.x + ((clickableMid.x - e.clientX)*0.15),
-			y: clickableMid.y + ((clickableMid.y - e.clientY)*0.15)
-		};
-	} else {
-		// Otherwise, follow the cursor
-		active = {
-			x: e.clientX,
-			y: e.clientY
-		}
-	}
-
-	// Animation loop to follow mouse cursor with a tweened delay
-	(function loop() {
-		x += easeInOutQuad(t) * (active.x - x);
-		y += easeInOutQuad(t) * (active.y - y);
-
-		x = Math.ceil(x * 100) / 100;
-		y = Math.ceil(y * 100) / 100;
-		
-		if (t < 1) {
-			t += 0.1;
-			container.style.transform = `translate(${x}px, ${y}px)`;
-			requestAnimationFrame(loop);
-		}
-	})();
-
-	function easeInOutQuad(t) {
-		return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-	}
-}
-
-// Checks if a node within the clickables list is being hovered
-function currentlyHoveredClickable(e) {
-
-	if (!clickables) return false;
-
-	let isHover, hoveredElem;
-	
-	// Iterate over every clickable to see if it intersects with cursor dot
-	for (let i = 0; i < clickables.length; i++) {
-		
-		if (isHover) break;
-
-		let currentClickable = clickables[i];
-		let boundBox = currentClickable.getBoundingClientRect();
-
-		if (!currentClickable) return false;
-		
-		// Checks to see if matched hovered elementis in scroll frame
-		isHover = (
-			(e.pageY <= boundBox.bottom) && 
-			(e.pageY >= boundBox.top) && 
-			(e.pageX >= boundBox.left) && 
-			(e.pageX <= boundBox.right)
-		);
-			
-		hoveredElem = currentClickable;
-
-		let topElt = document.elementFromPoint(boundBox.left, boundBox.top);
-		if (topElt) {
-			let overlay = hoveredElem.contains(topElt) || topElt.isSameNode(container);
-			
-			if (!overlay) isHover = false;
-		}
-	}
-
-	if (isHover) return hoveredElem;
-	return false;
-}
-
 
 /** Svelte store subscriptions **/
-
 isWorkScroll.subscribe(val => {
 	disabled = val;
 })
@@ -117,9 +21,91 @@ clickableStore.subscribe(value => {
 	clickables = value;
 });
 
+
+abstract class CursorDot {
+
+	static currentPosition = {
+		x: 0,
+		y: 0
+	};
+	static targetPosition = {
+		x: 0,
+		y: 0
+	};
+
+	// onMouseMove, set current mouse coords and detect if any clickables are hovered
+	static updateMouseCoords(e) {
+		// Dont do anything if its a touch mobile device
+		if ($isMobile) return;
+		// Intro animation for cursor dot
+		if (introDisabled) setTimeout(() => introDisabled = false, 500);
+
+		let clickable = false;
+
+		let cursor = window.getComputedStyle(e.target)["cursor"];
+		hover = (cursor === "pointer");
+
+		// clickable transition if mouse changes to pointer
+		if (cursor === "pointer") {
+
+			let currentlyHovering = document.elementFromPoint(e.clientX, e.clientY);
+			
+			let width = currentlyHovering.clientWidth;
+			let height = currentlyHovering.clientHeight;
+			
+			let clickableMid = {
+				x: (currentlyHovering.getBoundingClientRect().left + (width / 2)),
+				y: (currentlyHovering.getBoundingClientRect().top + (height / 2))
+			}
+
+			this.targetPosition = {
+				x: clickableMid.x + ((clickableMid.x - e.clientX)*0.15),
+				y: clickableMid.y + ((clickableMid.y - e.clientY)*0.15)
+			};
+		} else {
+			// Otherwise, follow the cursor
+			this.targetPosition = {
+				x: e.clientX,
+				y: e.clientY
+			}
+		}
+	}
+
+
+	// Animation loop for tweening (only call once)
+	static animate() {
+		let t = 0.4;
+
+		this.currentPosition.x += this.easeInOutQuad(t) * (this.targetPosition.x - this.currentPosition.x);
+		this.currentPosition.y += this.easeInOutQuad(t) * (this.targetPosition.y - this.currentPosition.y);
+		
+		// Fix js's weird rounding errors
+		this.currentPosition.x = Math.ceil(this.currentPosition.x * 100) / 100;
+		this.currentPosition.y = Math.ceil(this.currentPosition.y * 100) / 100;
+
+		container.style.transform = `translate3d(${this.currentPosition.x}px, ${this.currentPosition.y}px, 0px)`;
+
+		requestAnimationFrame(() => this.animate());
+	}
+
+	// Tweening function
+	static easeInOutQuad(t) {
+		return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+	}
+}
+
+
+// Run Animation Loop to begin tweening
+onMount(() => {
+	CursorDot.animate();
+})
+
+
 </script>
 
 
+
+<svelte:body on:mousemove = {(e) => CursorDot.updateMouseCoords(e)}/>
 
 <div class="dot-container active" 
 	bind:this={container}
